600bfba4588673d2f7923abf4e02db614a73e8b3
Sai Srilekha Balla <sballa@evoketechnologies.com>
2021-03-14 17:04:32 +0530

    My changes

--- demo/src/main/java/com/example/demo/EmployeeDaoJpaImplement.java
+++ demo/src/main/java/com/example/demo/EmployeeDaoJpaImplement.java
@@ -1,5 +1,61 @@
 package com.example.demo;
 
-public class EmployeeDaoJpaImplement {
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
 
+@Repository
+public class EmployeeDaoJpaImplement implements Dao<Employee> {
+
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Override
+    public void save(Employee employee) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public Employee load(int empid) {
+        EntityManager em = emf.createEntityManager();
+        Employee employee = em.find(Employee.class, empid);
+        em.close();
+        return employee;
+    }
+
+    @Override
+    public void delete(int empid) {
+        EntityManager em = emf.createEntityManager();
+        Employee employee = em.find(Employee.class, empid);
+
+        em.getTransaction().begin();
+        em.remove(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void update(Employee employee) {
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public List<Employee> loadAll() {
+        EntityManager em = emf.createEntityManager();
+        List<Employee> employees = em.createQuery("Select e from Employee e")
+                                 .getResultList();
+        em.close();
+        return employees;
+    }
 }
