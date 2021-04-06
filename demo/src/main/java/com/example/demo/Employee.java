d847ba544e08b239daadc620792d22bd1d7bd205
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-04-06 14:16:52 +0530

    Update Employee.java

--- demo/src/main/java/com/example/demo/Employee.java
+++ demo/src/main/java/com/example/demo/Employee.java
@@ -10,17 +10,17 @@ public class Employee {
 
 	@Id
 	@GeneratedValue
-	private int empid;
+	private int id;
 	private String name;
 	private String email;
 	private int phone;
 	private String created_by;
 	private Date created_on;
 	public long getEmpid() {
-		return empid;
+		return id;
 	}
-	public void setEmpid(int empid) {
-		this.empid = empid;
+	public void setid(int id) {
+		this.id = id;
 	}
 	public String getName() {
 		return name;
@@ -54,18 +54,18 @@ public class Employee {
 	}
 	@Override
 	public String toString() {
-		return "Employee [empid=" + empid + ", name=" + name + ", email=" + email + ", phone=" + phone + ", created_by="
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", created_by="
 				+ created_by + ", created_on=" + created_on + "]";
 	}
 	
-	public static Employee create(int empid,
+	public static Employee create(int id,
 	 String name,
 	 String email,
 	 int phone,
 	 String created_by,
 	 Date created_on) {
 		Employee employee= new Employee();
-		employee.setEmpid(empid);
+		employee.setid(id);
 		employee.setName(name);
 		employee.setEmail(email);
 		employee.setPhone(phone);
